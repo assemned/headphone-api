358805a647068fa6b82cce32a41847eeb7743274
assemned <assemned1@gmail.com>
2023-10-29 14:08:42 +0100

    error Solved

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5000;
+export const PORT = process.env.PORT || 3500;
 
 export const mongodbURL =
   "mongodb+srv://assemned88:Assemned88@headphones.z30kg6e.mongodb.net/headphones-database?retryWrites=true&w=majority";
